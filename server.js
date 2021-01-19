73e02bd62d21ae3adc91ce8626983870ab4db694
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-19 15:40:40 -0500

    Add logging for better debbuging

--- server.js
+++ server.js
@@ -6,11 +6,23 @@ dotenv.config();
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.start((ctx) => ctx.reply('Epic'));
-bot.command('tumae', (ctx) => ctx.reply('La tuya'));
-bot.hears('Oe bot', (ctx) => ctx.reply('Que quieres cabeza de cebolla, te vas a mamar tu muñequera'));
-bot.command('furry', (ctx) => ctx.reply('Unknown api: Have you considered commiting suicide?'));
-bot.command('porno', (ctx) => ctx.reply('Unknown api: Have you considered commiting suicide?'));
+bot.command('tumae', (ctx) => {
+  console.log(`/tumae request from ${ctx.from.username}`);
+  ctx.reply('La tuya');
+});
+bot.hears('Oe bot', (ctx) => {
+  console.log(`Heard Oe bot request from ${ctx.from.username}`);
+  ctx.reply('Que quieres cabeza de cebolla, te vas a mamar tu muñequera');
+});
+bot.command('furry', (ctx) => {
+  console.log(`/furry bot request from ${ctx.from.username}`);
+  ctx.reply('Unknown api: Have you considered commiting suicide?');
+});
+bot.command('porno', (ctx) => {
+  console.log(`/porno request from ${ctx.from.username}`);
+  ctx.reply('Unknown api: Have you considered commiting suicide?');
+});
 bot.launch();
 
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
