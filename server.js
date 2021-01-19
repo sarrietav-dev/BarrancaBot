eb252c1393e951cd22e7e6adbb197c54c3be54fc
Null-ct <66181951+Sebas721@users.noreply.github.com>
2021-01-19 12:08:41 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,5 +7,7 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.start((ctx) => ctx.reply('Epic'));
 bot.command('tumae', (ctx) => ctx.reply('La tuya'));
-
+bot.hears('Oe bot', (ctx) => ctx.reply('Que quieres cabeza de cebolla, te vas a mamar tu muñequera'));
+bot.command('furry', (ctx) => ctx.reply('Unknown api: Have you considered commiting suicide?'));
+bot.command('porno', (ctx) => ctx.reply('Unknown api: Have you considered commiting suicide?'));
 bot.launch();
